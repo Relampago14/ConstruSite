0854d098897148c93e5b67a41d06b1ebb3203650
Arthur Henry Martins Brito <131808877+aTu44@users.noreply.github.com>
2024-06-17 16:09:59 -0300

    filtro__tela__departamentos__js

---
+++ filtro.js
@@ -0,0 +1,13 @@
+document.getElementById('searchInput').addEventListener('keyup', function() {
+    let filter = this.value.toLowerCase();
+    let items = document.querySelectorAll('#itemList li');
+
+    items.forEach(function(item) {
+        let text = item.textContent.toLowerCase();
+        if (text.includes(filter)) {
+            item.style.display = '';
+        } else {
+            item.style.display = 'none';
+        }
+    });
+});
